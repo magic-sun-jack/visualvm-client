6c4d80d1fd6ca651c3a33565cae3ed30964acbd7
magic-ben <376955598qq@gmail.com>
2025-12-29 10:12:42 +0800

    更新版本号至1.0.16，调整构建信息以反映最新的构建时间和提交信息。

--- src/config/build-info.ts
+++ src/config/build-info.ts
@@ -1,13 +1,13 @@
 // 自动生成的构建信息
 export const buildInfo = {
-  "version": "1.0.15",
-  "buildTime": "2025-12-23T12:12:14.396Z",
+  "version": "1.0.16",
+  "buildTime": "2025-12-29T02:07:56.569Z",
   "git": {
-    "commitHash": "74eebfe",
-    "commitCount": "113",
+    "commitHash": "6da72cf",
+    "commitCount": "126",
     "branch": "main"
   },
-  "buildNumber": "113"
+  "buildNumber": "126"
 } as const
 
 export const getVersion = () => buildInfo.version
